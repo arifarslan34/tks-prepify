7925564e77a401ee594c4708e547c857d5abbecf
Cyber War 2050 <cyberwar2050@gmail.com>
2025-07-02 08:08:15 +0000

    Can you please add and implement firebase database.... details are as fo

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "tks-prepify.firebaseapp.com",
+  projectId: "tks-prepify",
+  storageBucket: "tks-prepify.firebasestorage.app",
+  messagingSenderId: "1078973522515",
+  appId: "1:1078973522515:web:b96cc234f376a94e9cdca6",
+  measurementId: "G-8QFLNX27DF"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, analytics };
